6843689c5ad29060af48bab8379e320328ffa5c1
Doroshenko Maksym <Doroshenko.Maksym@flyuia.com>
2019-08-14 13:25:13 +0300

    min startup server index file

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -1,1 +1,38 @@
+import { ApolloServer, gql } from 'apollo-server-express'
+import { WebApp } from 'meteor/webapp'
+import { getUser } from 'meteor/apollo'
+
 import ResolutionsSchema from '../../api/resolutions/Resolutions.graphql'
+
+const typeDefs = `
+type Query {
+    hi: String
+}
+`;
+
+const resolvers = {
+    Query: {
+        hi() {
+            return 'Matrix!'
+        }
+    }
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: async ({ req }) => ({
+        user: await getUser(req.headers.authorization)
+    })
+});
+
+server.applyMiddleware({
+    app: WebApp.connectHandlers,
+    path: '/graphql'
+});
+
+WebApp.connectHandlers.use('/graphql', (req, res) => {
+    if (req.method === 'GET') {
+        res.end()
+    }
+});
