c42ee93e9963193d48a77b6eef6b168a65eff5bd
Doroshenko Maksym <Doroshenko.Maksym@flyuia.com>
2019-08-14 11:56:32 +0300

    react and meteor connected

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -2,8 +2,29 @@ import React from 'react'
 import { Meteor } from 'meteor/meteor'
 import { render } from 'react-dom'
 
+import { Accounts } from 'meteor/accounts-base'
+import ApolloClient from 'apollo-boost'
+
+import { ApolloProvider } from '@apollo/react-hooks';
+
 import App from '../../ui/App'
 
+const client = new ApolloClient({
+    uri: '/graphql',
+    request: operation =>
+        operation.setContext(() => ({
+            headers: {
+                authorization: Accounts._storedLoginToken()
+            }
+        }))
+});
+
+const ApolloApp = () => (
+  <ApolloProvider client={client}>
+        <App />
+  </ApolloProvider>
+);
+
 Meteor.startup(() => {
-    render(<App />, document.getElementById('app'))
+    render(<ApolloApp />, document.getElementById('app'))
 });
