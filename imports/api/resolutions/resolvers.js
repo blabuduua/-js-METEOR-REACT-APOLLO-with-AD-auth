3ed4ec8b7ed48126d7bd2106b97789a29635320a
Doroshenko Maksym <Doroshenko.Maksym@flyuia.com>
2019-08-14 17:36:54 +0300

    add mongo collection, and return array resolutions from collection

--- imports/api/resolutions/resolvers.js
+++ imports/api/resolutions/resolvers.js
@@ -1,16 +1,13 @@
+import Resolutions from './resolutions'
+
+/*Resolutions.insert({
+    name: "test Res"
+});*/
+
 export default {
     Query: {
         resolutions() {
-            return [
-                {
-                    _id: "qwdqwdwqwd",
-                    name: "Matrixxx"
-                },
-                {
-                    _id: "123123123123123",
-                    name: "Matrixxx 2/0"
-                }
-            ]
+            return Resolutions.find({}).fetch();
         }
     }
 };
