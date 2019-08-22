a3db795c66d3baeba6944966101e8386ca5a3211
Doroshenko Maksym <Doroshenko.Maksym@flyuia.com>
2019-08-22 16:48:27 +0300

    AUTH DONE !!!!

--- imports/api/authenticate/authenticate.js
+++ imports/api/authenticate/authenticate.js
@@ -7,14 +7,7 @@ const authenticateData = Authenticate.find({}).fetch();
 if(authenticateData.length === 0){
     console.log('data  required');
 
-    Authenticate.insert({
-        url: "ldap://vsrvdc1-kbp.flyuia.com",
-        baseDN: "dc=flyuia,dc=com",
-        forbiddenDN: "",
-        userGroupsNames: "gr_portal_Slave_User",
-        adminGroupsNames: "",
-        blockGroupsNames: ""
-    });
+
 }else{
     console.log('data isset');
 
